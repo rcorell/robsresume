79e0f1afd60108e46e5ecf7bc0cdbe5cac617728
rcorell <rcorell@sadclown.com>
2013-02-18 22:25:45 -0600

    Converting to dynamic creation of job subsections

--- resume.js
+++ resume.js
@@ -3,106 +3,69 @@
  	Author: Rob Corell
  */
 
- $(document).ready(function() {
- 	$contactInfo 	= $( '#contactInfo' );
+// TODO: namespace
+
+var employmentData = {
+	"PayPal"	: 	{ 	
+						jobTitle : 				"Senior UI Developer",
+						employmentDuration : 	"2011-current",
+						projects : 				
+							{	project: {
+									projectName 		: 	"Log In with PayPal",
+									projectDescription 	: 	"Single sign-on commerce identity",
+									projectDetail		: 	"Log In with PayPal allows users to sign in to new websites using their PayPal credentials.  \
+															They can then skip the registration process, securely and seamlessly pay for products \
+															and, if they desire, consent to share information like their shipping address and phone number.",
+									projectTech			: 	"Implemented with JavaScript, jQuery, Node.js and Java." 
+								}
+							}
+				}
+};
+
+
+$(document).ready(function() {
+
+	$contactInfo 	= $( '#contactInfo' );
 	$contactToggle	= $( '#contactToggle' );
 
  	$contactInfo.hide();
  	$contactToggle.click( function() {
  			$contactInfo.toggle();
  		});
+
+
+	$experience = $("#experience");
+
+	jQuery.each( employmentData, function(companyName,job) {
+
+		function addElement( tag, cssClass, value, $target ) {
+			$( "<" + tag + "></" + tag +">" ).addClass( cssClass ).html( value ).appendTo( $target );
+		}
+
+
+		var $job = $( "<div></div>" ).addClass( "job" );
+		addElement( "span", "companyName", 			companyName + ", ", 	$job );
+		addElement( "span", "jobTitle",				job.jobTitle,			$job );
+		addElement( "span", "employmentDuration",	job.employmentDuration,	$job );
+
+		jQuery.each( job.projects, function(key,project) {
+			var $project = $( "<div></div>", { "class" : "project" } );
+			addElement( "span", "projectName", 			project.projectName + " ", 	$project );
+			addElement( "span", "projectDescription", 	project.projectDescription, $project );
+			addElement( "div", 	"projectDetail", 		project.projectDetail, 		$project );			
+			addElement( "div", 	"projectTech", 			project.projectTech, 		$project );	
+		
+			$project.appendTo( $job );
+		});
+
+		$job.appendTo( $experience );
+	});
+
+	//TODO: handle URL with "/full" at the end that displays all toggle areas
+	//if( /full$/.test(document.URL) || /showDetails$/.test(document.URL)	)
+	//	toggleAll();
+
  });
 
-		function expandHidable( index, hideContractionControl )
-		{
-			var hidable			= document.getElementById( "hidable" + index );
-			var hidableToggle	= document.getElementById( "hidableToggle" + index );
-
-			hidable.className = "show";
-			if( typeof hideContractionControl == "undefined" )
-				hidableToggle.innerHTML = "[&ndash;]";	
-			else
-				hidableToggle.innerHTML = "";
-		}
-		
-		function contractHidable( index )
-		{
-			var hidable			= document.getElementById( "hidable" + index );
-			var hidableToggle	= document.getElementById( "hidableToggle" + index );
-
-			hidable.className = "hide";	
-			hidableToggle.innerHTML = "[+]";				
-		}		
-		
-		function toggleHidable( index )
-		{
-			var hidable	= document.getElementById( "hidable" + index );		
-			
-			if( hidable.className == "hide" )
-				expandHidable( index );
-			else
-				contractHidable( index );
-		}
-
-		var toggleAllState	= "expand";
-		var numHidables		= 21;
-		var adobeExpanded	= false;
-				
-		function toggleAll()
-		{
-			var masterToggle = document.getElementById( "masterToggle" );
-			var adobeToggle = document.getElementById( "adobeToggle" );							
-			
-			if( toggleAllState == "expand" )
-			{
-				for( var i = 0; i < numHidables; i++ )	
-					expandHidable( i, "hideContractionControl" );					
-			
-				toggleAllState = "contract";
-				masterToggle.innerHTML = " hide&nbsp;details ";
-				
-				adobeExpanded = true;
-				adobeToggle.innerHTML = "";				
-			}
-			else
-			{
-				for( var i = 0; i < numHidables; i++ )	
-					contractHidable( i );					
-			
-				toggleAllState = "expand";	
-				masterToggle.innerHTML = "show&nbsp;details";
-				
-				adobeExpanded = false;
-				adobeToggle.innerHTML = "[+]";
-			}
-		}		
-		
-		var adobeHidables = [ 0, 1, 6, 7, 8, 9, 10, 11, 12, 13, 19 ];
-		function toggleAdobe()
-		{
-			var adobeToggle = document.getElementById( "adobeToggle" );
-			
-			if( adobeExpanded )
-			{
-				for( var i = 0; i < adobeHidables.length; i++ )
-					contractHidable( adobeHidables[i] );					
-					
-				adobeToggle.innerHTML = "[+]";				
-				
-				adobeExpanded = false;
-			}
-			else
-			{
-				for( var i = 0; i < adobeHidables.length; i++ )
-					expandHidable( adobeHidables[i], "hideContractionControl" );					
-					
-				adobeToggle.innerHTML = "[&ndash;]";
-				
-				adobeExpanded = true;
-			}		
-		}
-		
-		if( /full$/.test(document.URL) || /showDetails$/.test(document.URL)	)
-			toggleAll();
 
 
